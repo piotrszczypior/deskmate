d7118bbdbad5d5378607741d0e3e3f81bd2689dd
Piotr Szczypior <piotrekszczypior1@gmail.com>
2025-04-05 17:00:56 +0200

    FE: drag and drop floor configuration

--- deskmate-client/src/app/file-uploader/file-uploader.component.ts
+++ deskmate-client/src/app/file-uploader/file-uploader.component.ts
@@ -1,11 +1,11 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {FileUploaderDropZoneComponent} from './file-uploader-drop-zone/file-uploader-drop-zone.component';
-import {IconComponent} from '../icon/icon.component';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FileUploaderDropZoneComponent } from './file-uploader-drop-zone/file-uploader-drop-zone.component';
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
+import { UploadedImageComponent } from './images-drag-and-drop/uploaded-image.component';
 
 @Component({
   selector: 'app-file-uploader',
-  imports: [FileUploaderDropZoneComponent, IconComponent],
+  imports: [FileUploaderDropZoneComponent, CdkDropList, UploadedImageComponent, CdkDrag],
   templateUrl: './file-uploader.component.html',
   styleUrl: './file-uploader.component.scss',
 })
@@ -14,11 +14,15 @@ export class FileUploaderComponent {
   uploadedFilesEmitter = new EventEmitter<File[]>();
 
   @Output()
+  filesOrderChanged = new EventEmitter<CdkDragDrop<string[]>>();
+
+  @Output()
   deletedFileEmitter = new EventEmitter<File>();
 
   private _files: File[] = [];
 
   get files(): File[] {
+    console.log(this._files);
     return this._files;
   }
 
@@ -28,7 +32,16 @@ export class FileUploaderComponent {
   }
 
   deletedFile($event: File) {
-    this._files = this._files.filter(file => file !== $event);
+    this._files = this._files.filter((file) => file !== $event);
     this.deletedFileEmitter.emit($event);
   }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this._files, event.previousIndex, event.currentIndex);
+    this.filesOrderChanged.emit(event);
+  }
+
+  // confirmFloorsConfiguration($event: Event): void {
+  //   this.uploadedFilesEmitter.emit(this._files);
+  // }
 }
