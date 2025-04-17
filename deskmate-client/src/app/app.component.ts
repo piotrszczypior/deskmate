6425e19ca46aadf5cb93d7238950606efbba8da9
Piotr Szczypior <piotrekszczypior1@gmail.com>
2025-04-17 20:26:40 +0200

    header

--- deskmate-client/src/app/app.component.ts
+++ deskmate-client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
